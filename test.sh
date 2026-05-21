92dccfb605e0d525a49453dc1fc6d7a58ebec6f7
ravikadri <104390320+ravikadri@users.noreply.github.com>
2026-05-21 13:06:47 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-echo "Hello World!!456231"
+echo "Hello World Swathi"
 
-m365 login --authType identity --userName {guid}
+m365 login --authType identity --userName a928a515-d619-4992-9048-92f17ad7c219
 
-echo "Hello World!!7895932"
+echo "Hello World!!Ravi"
