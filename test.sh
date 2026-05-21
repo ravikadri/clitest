c9759a90162f3e8eac1798f920ff943887577256
ravikadri <104390320+ravikadri@users.noreply.github.com>
2026-05-21 13:16:37 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@ echo "Hello World Vihan"
 
 m365 login --authType identity --userName a928a515-d619-4992-9048-92f17ad7c219
 
+m365 entra approleassignment list --appDisplayName "dgw-ma-cli-test"
+
 m365 spo site list --type CommunicationSite --output json
 
 echo "Hello World!!Lishan"
