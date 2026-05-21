10ecd18c59eed0ab63a927f4ec372b3e0aea7da0
ravikadri <104390320+ravikadri@users.noreply.github.com>
2026-05-21 13:11:52 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
-echo "Hello World Swathi"
+echo "Hello World Vihan"
 
 m365 login --authType identity --userName a928a515-d619-4992-9048-92f17ad7c219
 
-echo "Hello World!!Ravi"
+m365 spo site list --type CommunicationSite --output json
+
+echo "Hello World!!Lishan"
