c8a5673831d080c84e015df3c4c93fca96cb6a2c
ravikadri <104390320+ravikadri@users.noreply.github.com>
2026-05-21 13:24:00 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
-echo "Hello World Vihan"
+echo "Hello World Kadri"
 
 m365 login --authType identity --userName a928a515-d619-4992-9048-92f17ad7c219
 
-m365 entra approleassignment list --appDisplayName "dgw-ma-cli-test"
+m365 spo site get --url "https://3lcmky.sharepoint.com/sites/VihanDevFixedIncome" --query "{Title:Title, Url:Url}"
 
-m365 spo site list --type CommunicationSite --output json
-
-echo "Hello World!!Lishan"
+echo "Mangalore"
