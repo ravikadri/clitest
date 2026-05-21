2eeaa0886b8e00d0e9ba8ee30f47b2f66b2aef6b
ravikadri <104390320+ravikadri@users.noreply.github.com>
2026-05-21 13:49:46 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
-echo "Hello World Kadri1231"
+echo "Tracy"
 
 m365 login --authType identity --userName a928a515-d619-4992-9048-92f17ad7c219
 
 
 
-m365 spo list list --webUrl https://3lcmky.sharepoint.com/sites/VihanDevFixedIncome
+m365 spo list list --webUrl https://3lcmky.sharepoint.com/sites/VihanDevFixedIncome  --properties "Id,Title"
+
+
+
 
 echo "Mangalore"
